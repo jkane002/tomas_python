61b901a63cfc8da8fd053ef01f976dc4dd2748c9
Tomas Derrow <tomasderrow@gmail.com>
2020-07-12 22:46:57 -0400

    Here the lesson 4 homework!

---
+++ homework/hw04.py
@@ -0,0 +1,8 @@
+name = input("What's your name? ");
+year = int(input("What year were you born? "));
+print(name , ", you were born in" , year);
+
+if year % 2 == 0:
+    print("That is an even year")
+else:
+    print("That is an odd year")
