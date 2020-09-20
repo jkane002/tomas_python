e86ec34f7ea60577ebf3ff9d34523d40cd45154b
Tomas Derrow <tomasderrow@gmail.com>
2020-09-20 17:02:14 -0400

    added template inheritance

--- tomas_project/blog/views.py
+++ tomas_project/blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+posts = [{'author': 'Tomas',
+ 'title': 'Blog post 1',
+ 'content': 'First post',
+ 'date_posted': 'September 13, 2020'},
+ {'author': 'Tomas',
+  'title': 'Blog post 2',
+  'content': 'First post',
+  'date_posted': 'September 13, 2020'}]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home </h1>')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html' , {'title': 'about'})
