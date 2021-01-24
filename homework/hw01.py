e28e5670b12132394933769c26876ecd4f283c21
Tomas Derrow <tomasderrow@gmail.com>
2021-01-24 16:16:44 -0500

    lesson 3

--- homework/hw01.py
+++ homework/hw01.py
@@ -1,30 +1,33 @@
-import random
-slist = []
+#import random
+#slist = []
 
 #make random intergers not input
 
-def inp():
-    stock1 = input("please input your first number: ")
-    slist.append(stock1)
-    stock2 = input("please input your second number: ")
-    slist.append(stock2)
-    stock3 = input("please input your third number: ")
-    slist.append(stock3)
-    stock4 = input("please input your fourth number: ")
-    slist.append(stock4)
-    stock5 = input("please input your fifth number: ")
-    slist.append(stock5)
-    print("------------------------------------------")
-    print(slist)
-    conf = input("is this the correct list? y/n ")
-    print("------------------------------------------")
-    if conf == "y":
-        print("Your list has been recorded")
-    elif conf == "n":
-        slist.clear()
-        inp()
-    else:
-        print(slist)
-        conf = input("is this the correct list? y/n ")
+#def inp():
+#    stock1 = input("please input your first number: ")
+#    slist1.append(stock1)
+#    stock2 = input("please input your second number: ")
+#    slist1.append(stock2)
+#    stock3 = input("please input your third number: ")
+#    slist1.append(stock3)
+#    stock4 = input("please input your fourth number: ")
+#    slist1.append(stock4)
+#    stock5 = input("please input your fifth number: ")
+#    slist1.append(stock5)
+#    print("------------------------------------------")
+#    print(slist1)
+#    conf = input("is this the correct list? y/n ")
+#    print("------------------------------------------")
+#    if conf == "y":
+#        print("Your list has been recorded")
+#    elif conf == "n":
+#        slist1.clear()
+#        inp()
+#    else:
+#        print(slist1)
+#        conf = input("is this the correct list? y/n ")
 
-inp()
+
+slist = [7,1,5,3,6,4]
+for :
+    print(i)
--- pygame_example/ex01.py
+++ pygame_example/ex01.py
@@ -1,20 +1,59 @@
 import pygame
 import os
 
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 60,50
 FPS = 60
 WIDTH, HEIGHT = 1500, 900
+vel = 5
+
+BORDER = pygame.Rect(WIDTH//2-5, 0, 10, HEIGHT)
+
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (55,40))
+YELLOW_SPACESHIP_SCALED = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
-RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (55,40))
+RED_SPACESHIP_SCALED = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
+red = pygame.Rect(1000, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("First game")
 
-def draw_window():
-    WIN.fill((40, 20, 150))
+YELLOW_SPACESHIP = pygame.transform.rotate(YELLOW_SPACESHIP_SCALED, 90)
+RED_SPACESHIP = pygame.transform.rotate(RED_SPACESHIP_SCALED, 270)
 
-    WIN.blit(YELLOW_SPACESHIP, (300, 100))
-    WIN.blit(RED_SPACESHIP, (500, 100))
+def yellow_handle_movement(keys_pressed, yellow):
+      if keys_pressed[pygame.K_a] and yellow.x - vel > 0:
+          yellow.x -= vel
+      # LEFT
+      if keys_pressed[pygame.K_d] and yellow.x + SPACESHIP_WIDTH - 20 + vel < BORDER.x:
+          yellow.x += vel
+      # RIGHT
+      if keys_pressed[pygame.K_w] and yellow.y - vel > 0:
+          yellow.y -= vel
+      # UP
+      if keys_pressed[pygame.K_s] and yellow.y + SPACESHIP_HEIGHT + 5 + vel < HEIGHT:
+          yellow.y += vel
+      # DOWN
+
+def red_handle_movement(keys_pressed, red):
+      if keys_pressed[pygame.K_LEFT] and red.x - vel > BORDER.x:
+          red.x -= vel
+      # LEFT
+      if keys_pressed[pygame.K_RIGHT] and red.x + vel + SPACESHIP_WIDTH - 10 < WIDTH:
+          red.x += vel
+      # RIGHT
+      if keys_pressed[pygame.K_UP] and red.y - vel + 5 > 0:
+          red.y -= vel
+      # UP
+      if keys_pressed[pygame.K_DOWN] and red.y + SPACESHIP_HEIGHT + 5 + vel < HEIGHT:
+          red.y += vel
+      # DOWN
+
+def draw_window(red, yellow):
+    WIN.fill((40, 20, 150))
+    pygame.draw.rect(WIN, (0,0,0), BORDER)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 def main():
@@ -25,7 +64,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        draw_window(red, yellow)
     pygame.quit()
 
 if __name__=="__main__":
