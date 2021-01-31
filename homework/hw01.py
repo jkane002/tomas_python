57027d19f0973b2cd7c2734e9aa2e1e7eb5b06af
Tomas Derrow <tomasderrow@gmail.com>
2021-01-31 16:53:52 -0500

    added homework

--- homework/hw01.py
+++ homework/hw01.py
@@ -1,33 +1,16 @@
-#import random
-#slist = []
-
-#make random intergers not input
-
-#def inp():
-#    stock1 = input("please input your first number: ")
-#    slist1.append(stock1)
-#    stock2 = input("please input your second number: ")
-#    slist1.append(stock2)
-#    stock3 = input("please input your third number: ")
-#    slist1.append(stock3)
-#    stock4 = input("please input your fourth number: ")
-#    slist1.append(stock4)
-#    stock5 = input("please input your fifth number: ")
-#    slist1.append(stock5)
-#    print("------------------------------------------")
-#    print(slist1)
-#    conf = input("is this the correct list? y/n ")
-#    print("------------------------------------------")
-#    if conf == "y":
-#        print("Your list has been recorded")
-#    elif conf == "n":
-#        slist1.clear()
-#        inp()
-#    else:
-#        print(slist1)
-#        conf = input("is this the correct list? y/n ")
-
+import random
 
 slist = [7,1,5,3,6,4]
-for :
-    print(i)
+minimumprice = slist[0]
+price = slist[0]
+difs = []
+
+for i in slist:
+    if i < minimumprice:
+         minimumprice = i
+    price = i
+    dif = price-minimumprice
+    difs.append(dif)
+    #print(dif)
+print("Your maximum profit is" , max(difs))
+    #[3,99,1,2]
